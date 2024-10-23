26660f5eeeafc5c414f8dbb7f8e7e6103040bd82
Vitor Medezani <vitormedezani@gmail.com>
2024-10-23 16:00:50 -0300

    add function and button

--- desafios/dsf01/index.php
+++ desafios/dsf01/index.php
@@ -5,7 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Desafio 02</title>
 </head>
+<header>
+  <h1>Trabalhando com números aleatórios</h1>
+  <?php
+
+  function NumeroAletorio()
+  {
+    return $numero_rdn = random_int(0, 100);
+  } 
+    echo NumeroAletorio();
+  ?>
+</header>
 <body>
-  
+  <br>
+  <input type="button" value="Gerar outro número" onclick="window.location.reload()">
 </body>
 </html>
