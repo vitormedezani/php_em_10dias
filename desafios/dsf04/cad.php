d97050cfb66b87f895ff48c044afca52d6a18c06
Vitor Medezani <vitormedezani@gmail.com>
2024-10-24 11:00:49 -0300

    creating algorithm

--- desafios/dsf04/cad.php
+++ desafios/dsf04/cad.php
@@ -5,6 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Desafio 05</title>
 </head>
+<header>
+  <?php 
+    $numero = 7469.254;
+    $prt_inteira = (int) $numero;
+    $prt_fracionada = round(($numero - $prt_inteira) * 1000) / 1000;
+    // $teste = $numero - $prt_inteira;
+    // $prt_fracionada = round((float) $prt_fracionada);
+    echo "Analisando número $numero";
+    echo "<br> Parte inteira: $prt_inteira";
+    echo "<br> Parte fracionária $prt_fracionada";
+    // echo "<br> Parte fracionária " . round($teste * 1000) / 1000;
+  ?>
+</header>
 <body>
   
 </body>
