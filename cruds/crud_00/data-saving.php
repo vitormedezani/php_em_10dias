4f46ba90a5c0b8ee3873c5e1534d215a12eca7d9
Vitor Medezani <vitormedezani@gmail.com>
2024-10-27 12:16:15 -0300

    fixing sql sintaxe to edit

--- cruds/crud_00/data-saving.php
+++ cruds/crud_00/data-saving.php
@@ -17,19 +17,43 @@ switch ($_REQUEST["crud_action"]) {
     $res = $conexao->query($sql);
 
     if ($res==true) {
-      echo "<script>alert('Cadastro feito com sucesso!!')</script>";
+      echo "<script>alert('Cadastro CRIADO com sucesso!!')</script>";
       echo "<script>location.href='?page=read';</script>";
     }
     else {
-      echo "<script>alert('Não foi possível realizar o cadastro!!')</script>";
+      echo "<script>alert('Não foi possível CRIAR o cadastro!!')</script>";
       echo "<script>location.href='?page=read';</script>";
     }
-    break;
+  break;
+  //
   case 'updating':
-    # code...
-    break;
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $senha = $_POST["senha"];
+    $data_nasc = $_POST["data_nasc"];
+
+    $sql = "UPDATE usuarios  SET 
+            nome='{$nome}', 
+            email='{$email}', 
+            senha='{$senha}', 
+            data_nasc='{$data_nasc}' 
+            WHERE
+            id=". $_REQUEST["id"];
+    $res = $conexao->query($sql);
+
+    if ($res==true) {
+      echo "<script>alert('Cadastro ALTERADO com sucesso!!')</script>";
+      echo "<script>location.href='?page=read';</script>";
+    }
+    else {
+      echo "<script>alert('Não foi possível ALTERAR o cadastro!!')</script>";
+      echo "<script>location.href='?page=read';</script>";
+    }
+
+  break;
+  //
   case 'deleting':
     # code...
-    break;
+  break;
 }
 ?>
