f6a88610173ff12270cc1e4e90d10c9739cc365a
Vitor Medezani <vitormedezani@gmail.com>
2024-10-27 08:43:04 -0300

    add create's the form structure

--- cruds/crud_00/create.php
+++ cruds/crud_00/create.php
@@ -1,1 +1,16 @@
 <h1>Novo Usuário</h1>
+<form action="?page=update" method="GET">
+  <label>Nome</label>
+  <input type="text" name="nome">
+  <!--  -->
+  <label>E-mail</label>
+  <input type="email" name="email">
+  <!--  -->
+  <label>Senha</label>
+  <input type="password" name="senha">
+  <!--  -->
+  <label>Data de Nascimento</label>
+  <input type="date" name="data_nasc">
+  <!--  -->
+  <button type="submit">Enviar</button>
+</form>
