2e08fe6f0bd6beb811009a65c0082422b870619f
Vitor Medezani <vitormedezani@gmail.com>
2024-10-27 10:40:47 -0300

    getting data of database and showing in a table

--- cruds/crud_00/read.php
+++ cruds/crud_00/read.php
@@ -1,1 +1,31 @@
 <h1>Listar Usuários</h1>
+<?php 
+  $sql = "SELECT * FROM usuarios";
+
+  $res = $conexao->query($sql);
+
+  //obtendo a quantidade de resultados da minha tabela
+  $qtd = $res->num_rows;
+
+  if ($qtd > 0) {
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>ID</th>";
+    echo "<th>NOME</th>";
+    echo "<th>EMAIL</th>";
+    echo "<th>DATA DE NASCIMENTO</th>";
+    echo "</tr>";
+    // $row está funcionado como uma array, recebendo os obejtos da tabela usuários 
+    while ($row = $res->fetch_object()) {
+      echo "<tr>";
+      echo "<td>$row->id</td>";
+      echo "<td>$row->nome </td>";
+      echo "<td>$row->email </td>";
+      echo "<td>$row->data_nasc </td>";
+      echo "</tr>";
+    }
+    echo "</table>";
+  } else {
+    echo "<p class='alert'>Não foi possível encontrar resultados!!</p>";
+  }
+?>
