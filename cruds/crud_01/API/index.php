db770f90771d4ee50823f867dc8238d515ca5a07
Vitor Medezani <vitormedezani@gmail.com>
2024-10-28 17:59:58 -0300

    add base of api

--- cruds/crud_01/API/index.php
+++ cruds/crud_01/API/index.php
@@ -1,3 +1,40 @@
 <?php 
-  echo "Hello World!";
+header('Acess-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+date_default_timezone_set("America/Sao_Paulo");
+
+//declarando a array path
+if (isset($_GET["path"])) {
+  # code...
+  $path = explode("/", $_GET["path"]);
+} else {
+  echo "Caminho não existe";
+  exit;
+}
+
+//lendo o caminho, ações e ou parâmetros
+if (isset($path[0])) {
+  $api = $path[0];
+} else {
+  echo "Caminho não existe";
+  exit;
+}
+//indentificando o ação
+if (isset($path[1])) {
+  $acao = $path[1];
+} else {
+  $acao = '';
+}
+//identificando o parâmetro
+if (isset($path[2])) {
+  $parametro = $path[2];
+} else {
+  $parametro = '';
+}
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+var_dump($method);
+
 ?>
