ac580d50ffffc8a1727a6e3058f1f6b4e50015e2
Vitor Medezani <vitormedezani@gmail.com>
2024-10-29 11:24:53 -0300

    indenting the code

---
+++ cruds/crud_01/API/api/usuarios/get.php
@@ -0,0 +1,44 @@
+<?php 
+  if ($api == 'usuarios') {
+    if ($method == "GET") {
+
+      switch (true) {
+        //verificando o caminho
+        case ($acao == '' && $parametro == ''):
+          
+          echo json_encode(['ERRO' => 'Caminho não encontrado']);
+        break;
+  
+        //listando todos os registros do banco
+        case ($acao == 'lista' && $parametro == ''):
+          
+          $db = DB::connect();
+          $resulatdo = $db->prepare("SELECT * FROM usuarios ORDER BY nome");
+          $resulatdo->execute();
+          $obj = $resulatdo->fetchAll(PDO::FETCH_ASSOC);
+          
+          if ($obj) {
+            echo json_encode(['dados' => $obj]);
+          } else {
+            echo json_encode(['dados' => 'Não exsitem dados para retornar']);
+          }
+        break;
+
+        //listando um registro espcifico pelo id
+        case ($acao == 'lista' && $parametro != ''):
+          
+          $db = DB::connect();
+          $resulatdo = $db->prepare("SELECT * FROM usuarios WHERE id={$parametro}");
+          $resulatdo->execute();
+          $obj = $resulatdo->fetchObject();
+          
+          if ($obj) {
+            echo json_encode(['dados' => $obj]);
+          } else {
+             echo json_encode(['dados' => 'Não exsitem dados para retornar']);
+          }
+        break;
+      }     
+    }
+  }
+?>
