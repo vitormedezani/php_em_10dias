ed51c9bc3b0e8badb70f98d2e56471064cd61741
Vitor Medezani <vitormedezani@gmail.com>
2024-10-29 12:22:27 -0300

    add file to method post

---
+++ cruds/crud_01/API/api/usuarios/post.php
@@ -0,0 +1,49 @@
+<?php 
+  switch (true) {
+    //verificando o caminho
+    case ($acao == '' && $parametro == ''):
+      echo json_encode(['ERRO' => 'Caminho não encontrado']);
+      break;  
+    case ($acao == 'adicionar' && $parametro == ''):
+      // .= mantém o valor anterior e adicona outro valor, basicamente serve pra quebrar a linha
+      //AUTOMATIZANDO a inserção de dados
+      $sql = "INSERT INTO usuarios (";
+      //automatizando os campos
+      $contador = 1;
+      foreach (array_keys($_POST) as $indice) {
+        //retirando a vírgula do último loop
+        if (count($_POST) > $contador) {
+          $sql .= "{$indice},";
+        } else {
+          $sql .= "{$indice}";
+        }
+        $contador ++;
+      }
+      $sql .= ") VALUES (";
+      //automatizando os valores
+      $contador = 1;
+      foreach (array_values($_POST) as $valor) {
+        //retirando a vírgula do último loop
+        if (count($_POST) > $contador) {
+          $sql .= "'{$valor}',";
+        } else {
+          $sql .= "'{$valor}'";
+        }
+        $contador ++;
+      }
+      //encerrando alinha de texto do sql
+      $sql .= ")";
+
+      $db = DB::connect();
+      $resulatdo = $db->prepare($sql);
+      $executar = $resulatdo->execute();
+
+      if ($executar) {
+        echo json_encode(['dados' => 'Dados foram inderidos com sucesso.']);
+      } else {
+        echo json_encode(['dados' => 'Houve algum erro ao inserir os dados!!']);
+      }
+
+      break;
+  }
+?>
