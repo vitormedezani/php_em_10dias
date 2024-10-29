abe7c1c3415ec9edd611c775038bcb7df2c1df69
Vitor Medezani <vitormedezani@gmail.com>
2024-10-29 14:23:27 -0300

    add redirection to put and delete

--- cruds/crud_01/API/api/usuarios/usuarios.php
+++ cruds/crud_01/API/api/usuarios/usuarios.php
@@ -1,14 +1,21 @@
 <?php
 //verificando caminho da api
+var_dump($_SERVER['REQUEST_METHOD']);
 if ($api == 'usuarios') {
-  //verificando os metódos  
-  switch ($method) {
-    case 'GET':
+  //verificando os metódos 
+  switch (true) {
+    case ($method == 'GET'):
       include __DIR__.'/get.php';
       break;
-    case 'POST':
+    case ($method == 'POST'):
       include __DIR__.'/post.php';
       break;
+    case ($_SERVER['REQUEST_METHOD'] === 'PUT'):
+      include __DIR__.'/put.php';
+      break;
+    case ($_SERVER['REQUEST_METHOD'] === 'DELETE'):
+      include __DIR__.'/delete.php';
+      break;    
     default:
       echo json_encode(['ERRO' => 'Caminho não encontrado']);
       break;
