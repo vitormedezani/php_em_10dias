ee46e5f1b2351de0b86aff6ee6d938de41db4271
Vitor Medezani <vitormedezani@gmail.com>
2024-10-29 15:46:26 -0300

    add method put/insert

--- cruds/crud_01/API/api/usuarios/put.php
+++ cruds/crud_01/API/api/usuarios/put.php
@@ -1,16 +1,47 @@
 <?php
-var_dump("teste"); 
   switch (true) {
+    
     case ($acao == '' && $parametro == ''):
       echo json_encode(['ERRO' => 'Caminho não encontrado']);
-      break;
+    break;
+
     case ($acao == 'update' && $parametro == '' ):
       echo "teste";
       echo json_encode(['ERRO' => 'É necessário informar o id do usuário']);
-      break;
+    break;
+    
     case ($acao == 'update' && $parametro != ''):
-      var_dump($_POST);
-      break;
+      array_shift($_POST);
+      
+      // "UPDATE usuarios SET nome = 'novo nome', telefone = 'novo tel' WHERE id=2"
+
+      $sql = "UPDATE usuarios SET ";
+      
+      //automatizando o comando sql de UPDATE
+      $contador = 1;
+      foreach (array_keys($_POST) as $indice) {
+        
+        if (count($_POST) > $contador) {
+          $sql .= "{$indice} = '{$_POST[$indice]}', ";
+        } else {
+          $sql .= "{$indice} = '{$_POST[$indice]}' ";
+        }
+        $contador ++;
+      }
+      $sql .= "WHERE id={$parametro} ";
+
+      $db = DB::connect();
+      $resultado = $db->prepare($sql);
+      $executar = $resultado->execute();
+
+      if ($executar) {
+        echo json_encode(['dados' => 'Dados atualizados com sucesso.']);
+      } else {
+        echo json_encode(['dados' => 'Houve algum erro ao atualizar os dados!!']);
+      }
+
+    break;
+
     default:
         echo json_encode(['ERRO' => 'Erro interno']);
     break;
