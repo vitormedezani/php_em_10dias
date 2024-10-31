6a7aea4ec69e2f1b4fa549539b9e83f6b4d1a1b1
Vitor Medezani <vitormedezani@gmail.com>
2024-10-31 12:09:43 -0300

    add connection w/ api

---
+++ cruds/crud_01/public/scripts/api.js
@@ -0,0 +1,14 @@
+const APIBASE = "http://localhost/php_em_10dias/cruds/crud_01/API"
+
+async function apiListarUsuarios() {
+  try {
+   const response = await fetch("${APIBASE}/usuarios/lista", {
+      method: "GET",
+   })
+
+   const data = await response.json()
+   return data 
+  } catch (error) {
+    console.error(error)
+  }
+}
