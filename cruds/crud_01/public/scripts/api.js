bd1b2492bd81fcf1d772db4e96fc0d853c907a93
Vitor Medezani <vitormedezani@gmail.com>
2024-10-31 18:29:44 -0300

    add path to api update method

--- cruds/crud_01/public/scripts/api.js
+++ cruds/crud_01/public/scripts/api.js
@@ -2,7 +2,7 @@ const APIBASE = "http://localhost/php_em_10dias/cruds/crud_01/API"
 
 async function apiListarUsuarios() {
   try {
-   const response = await fetch(APIBASE+"/usuarios/lista/", {
+   const response = await fetch(`${APIBASE}/usuarios/lista/`, {
      method: "GET",
    })
 
@@ -15,7 +15,21 @@ async function apiListarUsuarios() {
 
 async function apiCadastrarUsuario(formulario) {
   try {
-    const response = await fetch(APIBASE + "/usuarios/adicionar/", {
+    const response = await fetch(`${APIBASE}/usuarios/adicionar/`, {
+      method: "POST",
+      body: formulario,
+    })
+
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+async function apiModificarUsuario(id, formulario) {
+  try {
+    const response = await fetch(`${APIBASE}/usuarios/adicionar/${id}`, {
       method: "POST",
       body: formulario,
     })
