7b00bd71a768d51934d5c37fb90ac3532a4a29d4
Vitor Medezani <vitormedezani@gmail.com>
2024-10-31 13:53:13 -0300

    fixing api url

--- cruds/crud_01/public/scripts/api.js
+++ cruds/crud_01/public/scripts/api.js
@@ -2,8 +2,8 @@ const APIBASE = "http://localhost/php_em_10dias/cruds/crud_01/API"
 
 async function apiListarUsuarios() {
   try {
-   const response = await fetch("${APIBASE}/usuarios/lista", {
-      method: "GET",
+   const response = await fetch(APIBASE+"/usuarios/lista/", {
+     method: "GET",
    })
 
    const data = await response.json()
