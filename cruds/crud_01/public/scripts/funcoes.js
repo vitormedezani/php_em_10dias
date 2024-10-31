a1f768a686f1a7538b18cc0f4f8bc0d057b8fc98
Vitor Medezani <vitormedezani@gmail.com>
2024-10-31 15:53:15 -0300

    re doing tables

--- cruds/crud_01/public/scripts/funcoes.js
+++ cruds/crud_01/public/scripts/funcoes.js
@@ -1,6 +1,20 @@
 function listarUsuarios() {
   apiListarUsuarios().then(total => {
-    console.log(total)
+    document.querySelectorAll('main table#lista tbody tr').forEach(linha => linha.remove())
+    total.dados.map((item) => {
+      var lista = document.querySelector('table#lista tbody')
+      lista.innerHTML += `<tr>
+      <td>${item.nome}</td>
+      <td>${item.email}</td>
+      <td>${item.cidade}</td>
+      <td>${item.estado}</td>
+      <td>${item.telefone}</td>
+      <td>
+        <button onClick=""><ion-icon name="trash-outline"></ion-icon></button>
+        <button onClick=""><ion-icon name="create-outline"></ion-icon></button>
+      </td>
+      </tr>`
+    })
   })
 }
 
